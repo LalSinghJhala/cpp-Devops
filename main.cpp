276ecd0e85e492f32d34965316a42862c7266fb5
Lal Singh Jhala <lalsinghjhala1999@gmail.com>
2025-12-07 13:12:42 +0530

    second commit

--- main.cpp
+++ main.cpp
@@ -3,5 +3,6 @@ using namespace std;
 
 int main() {
     cout << "Hello from C++ App running inside Docker & Kubernetes!" << endl;
+    cout << "today on 7 dec 2025 i learned git and docker";
     return 0;
 }
